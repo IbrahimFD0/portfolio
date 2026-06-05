eb036da414782a9bd8482cc4f4e5b515b796c67c
IbrahimFD0 <bylordmc@protonamil.com>
2026-06-05 16:48:53 +0200

    Finished Hero section.

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,106 @@
+import { Download } from "lucide-react";
+import React from "react";
+import hero from "../assets/hero.png";
+
+const HeroSection = () => {
+  return (
+    <section className="relative w-full" data-aos="zoom-in-up">
+      <div className="absolute top-0 inset-x-0 h-64 flex items-start">
+        <div
+          className="h-24 w-2/3 bg-linear-to-br from[#8F00FF]
+        blur-2xl invisible opacity-40"
+        ></div>
+        <div
+          className="h-20 w-3/4 bg-linear-to-r from[#8F00FF]
+        opacity-40 blur-2xl"
+        ></div>
+      </div>
+      <div
+        className="grid lg:grid-cols-2 gap-10 relative pt-24 
+      lg:max-w-none max-w-2xl md:max-w-3xl mx-auto"
+      >
+        <div className="lg:py-6">
+          <div className="text-center lg:text-left">
+            <h1
+              className="pt-4 text-white font-bold
+            text-4xl md:text-5xl lg:text-6xl
+            "
+            >
+              Hi, I'm{" "}
+              <span className="text-transparent bg-clip-text bg-linear-to-r from-primary to-purple-200">
+                {" "}
+                Ibrahim
+              </span>
+              👋
+            </h1>
+          </div>
+          <p className="text-gray-300 pt-8 text-center lg:text-left mx-auto max-w-xl">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
+            faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi
+            pretium tellus duis convallis. Tempus leo eu aenean sed diam urna
+            tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas.
+            Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut
+            hendrerit semper vel class aptent taciti sociosqu. Ad litora
+            torquent per conubia nostra inceptos himenaeos.
+          </p>
+          <div className="flex items-center gap-3 pt-9 flex-col sm:flex-row sm:w-max lg:mx-0">
+            <button className="px-6 md:px-7 py-3 rounded-full relative group w-full sm:w-max flex justify-center">
+              <span
+                className="absolute inset-0 rounded-3xl group-hover:scale-105
+              origin-center transition-all ease-in-out bg-primary border-2 border-transparent"
+              ></span>
+              <span className="relative flex items-center justify-center text-white">
+                Hire Me
+              </span>
+            </button>
+            <button className="border border-purple-700 px-6 md:px-7 py-3 rounded-full relative group w-full sm:w-max flex justify-center">
+              <div className="hover:sclae-105 transition-all ease-in-out flex justify-center items-center relative">
+                <div className="svg-container">
+                  <Download size={18} className="text-primary" />
+                  <div className="download-loader text-white hidden"></div>
+                </div>
+                <a
+                  href="/resume.pdf"
+                  download="resume.pdf"
+                  className="pl-2 text-primary"
+                >
+                  Download resume
+                </a>
+              </div>
+            </button>
+          </div>
+        </div>
+        <div className="lg:h-full md:flex">
+          <div
+            className="flex w-full h-96 min-h-96
+          lg:min-h-[none] lg:w-full lg:h-full items-center relative"
+          >
+            <div
+              className="absolute z-0 top-1/2 -translate-y-1/2 w-5/6 right-0 h-[calc(80%+20px)] bg-linear-to-r opacity-25 
+            from-[#8F00FF] to-primary blur-2xl"
+            ></div>
+            <div
+              className="absolute h-full z- p-2 -translate-y-1/2 top-1/2 lg:rigth-3
+            md:rigth-40 sm:rigth-16
+            rounded-[30%_70%_70%_30%/30%_30%_70%_70%]
+            shadow-lg border border-purple-700"
+            >
+              <img
+                src={hero}
+                alt="hero picture"
+                width="500"
+                height="auto"
+                loading="lazy"
+                className="w-full h-full
+                rounded-[30%_70%_70%_30%/30%_30%_70%_70%]
+                object-cover"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
