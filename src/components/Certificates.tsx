ed52eeebfa6cf17a798c170b05c55c1e38d66f15
IbrahimFD0 <bylordmc@protonamil.com>
2026-06-07 14:35:00 +0200

    Updated Certificates.tsx imports.

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -1,9 +1,4 @@
-import {
-  Award,
-  Calendar,
-  ExternalLink,
-  ShieldCheck,
-} from "lucide-react";
+import { Award, Calendar, ExternalLink, ShieldCheck } from "lucide-react";
 import React from "react";
 import { BiCertification } from "react-icons/bi";
 
