9170de1ac3b4418cfc9d926e3210f89a408e2ec2
IbrahimFD0 <bylordmc@protonamil.com>
2026-06-08 09:20:57 +0200

    Added Footer component.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      className="mt-8 border z-10 border-t-[#33353F]
+  border-l-transparent border-r-transparent text-white"
+    >
+      <div className="container p-4 md:p-12 justify-between flex flex-col md:flex-row items-center md:gap-0">
+        <div className="text-white text-2xl font-black cursor-pointer">
+          PORTFOLIO<span className="text-primary">.</span>
+        </div>
+        <p className="text-slate-600 text-sm md:text-base">
+          All rigths reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
